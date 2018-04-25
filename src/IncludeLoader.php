93393dd3b563fc7a3e04145e2995a7154bd64b14
Marcelo <marceloaldo@gmail.com>
2018-04-25 09:12:15 -0300

    Agrega trait para eadge loading de relaciones.

---
+++ src/IncludeLoader.php
@@ -0,0 +1,29 @@
+<?php
+namespace Spys\ApiHelper;
+
+use League\Fractal\TransformerAbstract;
+
+trait IncludeLoader
+{
+    /**
+     * Permite el eadger loading de los includes permitidos por el transformer
+     *
+     * @param $includeString
+     * @param TransformerAbstract $transformer
+     * @return mixed
+     */
+    public function withIncludes($includeString, TransformerAbstract $transformer)
+    {
+        $keys = explode(',', $includeString);
+
+        $toApply = [];
+
+        foreach ($keys as $key) {
+            if (in_array($key, $transformer->getAvailableIncludes())) {
+                $toApply[] = $key;
+            }
+        }
+
+        return $this->with($toApply);
+    }
+}
